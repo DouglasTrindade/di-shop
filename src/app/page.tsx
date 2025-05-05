ee23e993d8fde95c1309ffdf6e708b3b726f7708
Douglas Trindade <douglastrindadev@gmail.com>
2025-05-05 08:00:37 -0300

    wip; next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default async function Home() {
-  return <h1>hi</h1>;
+"use client";
+
+import { login } from "@/lib/actions/auth";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Você não está logado</p>
+      <button
+        className="bg-indigo-500 px-3 py-2 rounded-2xl cursor-pointer"
+        onClick={() => login()}
+      >
+        Logar com Github
+      </button>
+    </div>
+  );
 }
